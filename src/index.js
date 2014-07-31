6fe053bda8bc8972f546ae777f837ff237335860
Samy Pessé <samypesse@gmail.com>
2014-07-30 21:37:08 -0700

    Add base logic for sync

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-define(function(coffeeScript) {
+define([
+    "src/sync"
+], function(FileSync) {
     var commands = codebox.require("core/commands");
     var File = codebox.require("models/file");
 
@@ -6,8 +8,18 @@ define(function(coffeeScript) {
         id: "editor.collaboration.toggle",
         title: "Editor: Toggle Collaboration",
         context: ["editor"],
-        run: function(args, context) {
+        run: function(args, editor) {
+            // Turn off sync on this file
+            if (editor.sync) {
+                editor.sync.stop();
+                editor.sync = null;
+                return;
+            }
 
+            editor.sync = new FileSync({
+                file: editor.mode
+            });
+            editor.sync.bindEditor(editor);
         }
     });
 });
